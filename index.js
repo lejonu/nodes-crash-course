6e6c47753d829964640b537c52c69445fcd60102
lejonu <leonardo.ferax@gmail.com>
2020-04-30 15:53:05 -0300

    The Laptop project part 2

--- index.js
+++ index.js
@@ -22,11 +22,48 @@ const server = http.createServer( ( req, res) => {
 
     if ( pathName === '/products' || pathName === '/') {
         res.writeHead( 200, { 'Content-type': 'text/html'} )
-        res.end( 'This is the PRODUCTS page' )
+
+        fs.readFile( `${ __dirname}/templates/template-overview.html`, 'utf-8', ( err, data ) =>{
+
+            let overviewOutput = data
+
+            fs.readFile( `${ __dirname}/templates/template-card.html`, 'utf-8', ( err, data ) =>{
+
+                const cardsOutput = laptopData.map( elem => replaceTemplate( data, elem )).join( '' )
+
+                overviewOutput = overviewOutput.replace( '{%CARDS%}', cardsOutput )
+
+                res.end( overviewOutput )
+            })
+
+        })
+
+
+        // res.end( 'This is the PRODUCTS page' )
     } 
     else if( pathName === '/laptop' && id < laptopData.length ){
         res.writeHead( 200, { 'Content-type': 'text/html'} )
-        res.end( `This is the LAPTOP ${id}` )
+
+        fs.readFile( `${ __dirname}/templates/template-laptop.html`, 'utf-8', ( err, data ) =>{
+            const laptop = laptopData[ id ]
+
+            const output = replaceTemplate( data, laptop )
+
+            res.end( output )
+        })
+
+
+        // res.end( `This is the LAPTOP ${id}` )
+    }
+    // IMAGES
+    else if( (/\.(jpg|jpeg|png|gif)$/i).test(pathName) ){
+
+        fs.readFile( `${ __dirname}/data/img${pathName}`, ( err, data ) => {
+            res.writeHead( 200, { 'Content-type': 'image/jpg'} )
+            res.end( data )
+        })
+
+        // res.end( `This is the LAPTOP ${id}` )
     }
     else {
         res.writeHead( 404, { 'Content-type': 'text/html'} )
@@ -38,3 +75,17 @@ server.listen( 1337, '127.0.0.1', () =>{
     console.log( 'Listening for requesting now' )
 })
 
+
+function replaceTemplate( originalHtml, laptop){
+    let output = originalHtml.replace( /{%PRODUCTNAME%}/g, laptop.productName )
+    output = output.replace( /{%IMAGE%}/g, laptop.image )
+    output = output.replace( /{%PRICE%}/g, laptop.price )
+    output = output.replace( /{%SCREEN%}/g, laptop.screen )
+    output = output.replace( /{%CPU%}/g, laptop.cpu )
+    output = output.replace( /{%STORAGE%}/g, laptop.storage )
+    output = output.replace( /{%RAM%}/g, laptop.ram )
+    output = output.replace( /{%DESCRIPTION%}/g, laptop.description )
+    output = output.replace( /{%ID%}/g, laptop.id )
+
+    return output
+}
